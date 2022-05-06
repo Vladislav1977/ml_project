c00072520ce56518c2b3a30446fb16b4d6b51816
Vladislav1977 <vladson1077mail.ru>
2022-05-06 22:09:12 +0300

    add function Args. description to pipeline modele

--- src/forest_ml/pipeline.py
+++ src/forest_ml/pipeline.py
@@ -5,6 +5,22 @@ from sklearn.preprocessing import StandardScaler
 
 def make_model(scaler=True, log_penalty="l1", log_max_iter=1000, log_C=1.0, random_state=42):
 
+    """Logistic Regression classifier.
+
+            Args:
+                scaler: bool, default=True
+                    Data standartization.
+                log_penalty: string, default="L1"
+                    Type of regularization
+                log_max_iter: int, default=1000
+                    Maximum number of iterations taken for the solvers to converge.
+                log_C: float, default=1.0
+                    Inverse of regularization strength; must be a positive float.
+                    Smaller values specify stronger regularization.
+                random_state: int, default=42
+                    Random state.
+            """
+
     steps = []
     if scaler:
         steps.append(("scaler", StandardScaler()))
