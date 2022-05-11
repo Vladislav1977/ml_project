4e4daeae86193760f1b066ac30b4b7b4e43ffe5e
Vladislav1977 <vladson1077mail.ru>
2022-05-11 17:41:02 +0300

    change pipeline script

--- src/forest_ml/pipeline.py
+++ src/forest_ml/pipeline.py
@@ -1,33 +1,19 @@
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
+from sklearn.ensemble import RandomForestClassifier
 
 
-def make_model(scaler, log_penalty, log_max_iter, log_c, random_state):
+def make_model(scaler, log_max_depth, log_n_estimators, random_state=42):
 
-    """Logistic Regression classifier.
-
-            Args:
-                scaler: bool, default=True
-                    Data standartization.
-                log_penalty: string, default="L1"
-                    Type of regularization
-                log_max_iter: int, default=1000
-                    Maximum number of iterations taken for the solvers to converge.
-                log_C: float, default=1.0
-                    Inverse of regularization strength; must be a positive float.
-                    Smaller values specify stronger regularization.
-                random_state: int, default=42
-                    Random state.
-            """
 
     steps = []
     if scaler:
         steps.append(("scaler", StandardScaler()))
     steps.append(
-        ("regresion", LogisticRegression(
-            penalty=log_penalty, C=log_c, max_iter=log_max_iter, random_state=random_state
+            ("RandomForest", RandomForestClassifier(
+                max_depth=log_max_depth, n_estimators=log_n_estimators,
+                random_state=random_state)
+            )
         )
-         )
-    )
     return Pipeline(steps)
